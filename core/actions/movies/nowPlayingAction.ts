15bd61b18da82596df1bb1a2f9fa288b11f5be3a
emanuelhardwell <emanuelnoe16@gmail.com>
2026-01-22 23:28:16 -0600

    feat: Create MovieMapper and interface Movie

--- core/actions/movies/nowPlayingAction.ts
+++ core/actions/movies/nowPlayingAction.ts
@@ -1,10 +1,14 @@
 import { movieApi } from "@/core/api/movieApi";
 import { MovieDBResponse } from "@/infrastructure/interfaces/movieDbResponse";
+import { MovieMapper } from "@/infrastructure/mappers/movieMapper";
 
 export const nowPlayingAction = async () => {
   try {
     const res = await movieApi.get<MovieDBResponse>("/now_playing");
-    console.log(res.data);
+    const movies = res.data.results.map((movie) =>
+      MovieMapper.fromMovieDBtoMovie(movie)
+    );
+    console.log(movies);
   } catch (error) {
     console.log(error);
   }
---
+++ infrastructure/mappers/movieMapper.ts
@@ -0,0 +1,16 @@
+import { Movie } from "../interfaces/Movie";
+import { Result } from "../interfaces/movieDbResponse";
+
+export class MovieMapper {
+  static fromMovieDBtoMovie(movie: Result): Movie {
+    return {
+      id: movie.id,
+      title: movie.title,
+      description: movie.overview,
+      releaseDate: new Date(movie.release_date),
+      rating: movie.vote_average,
+      poster: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      backdrop: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
+    };
+  }
+}
